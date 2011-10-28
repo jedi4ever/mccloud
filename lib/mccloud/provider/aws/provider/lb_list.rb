7e87aed13f320e35b0b1a1d072321429ec0ac20d
Niels Laukens <niels.laukens@vrtmedialab.be>
2011-10-28 12:09:18 +0200

    Allow AWS-credentials to be overridden for load-balancers as well

--- lib/mccloud/provider/aws/provider/lb_list.rb
+++ lib/mccloud/provider/aws/provider/lb_list.rb
@@ -7,7 +7,7 @@ module Mccloud
           env.logger.info("#{selection} - #{options}")
 
           env.logger.info("Looking for loadbalancers in region #{@region}")
-          elb=Fog::AWS::ELB.new({:region => @region})
+          elb=Fog::AWS::ELB.new({:region => @region}.merge(@options))
           elb.load_balancers.each do |lb|
             env.ui.info "Id #{lb.id} - DNS #{lb.dns_name} - Zones: #{lb.availability_zones.join(',')} - Instances #{lb.instances.join(',')} "
           end
