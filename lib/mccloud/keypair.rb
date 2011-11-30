ceec2dcb7e5b0d4a56902a34e406b27ae5ec6f3b
Patrick Debois <Patrick.Debois@jedi.be>
2011-11-30 21:26:10 +0100

    added logic to generate keypair and check if exists?

--- lib/mccloud/keypair.rb
+++ lib/mccloud/keypair.rb
@@ -1,3 +1,5 @@
+require 'mccloud/util/sshkey'
+
 module Mccloud
   class Keypair
     attr_accessor :public_key_path
@@ -11,6 +13,32 @@ module Mccloud
       @private_key_path=nil
       @public_key_path=nil
       return self
+    end
+
+    def exists?
+      return false unless File.exists?(@public_key_path)
+      return false unless File.exists?(@private_key_path)
+      return true
+    end
+
+    def generate
+      if exists?
+        env.ui.info "Keypair: #{@name} already exists"
+        env.ui.info "- #{@public_key_path}"
+        env.ui.info "- #{@private_key_path}"
+      end
+        env.ui.info "Generating Keypair: #{@name}"
+        env.ui.info "- #{@public_key_path}"
+        env.ui.info "- #{@private_key_path}"
+        env.ui.info ""
+        env.ui.warn "Make sure you make a backup!!"
+        rsa_key=::Mccloud::Util::SSHKey.generate({ :comment => "Generate #{@name}"})
+        begin
+          File.open(@public_key_path,'w'}{|f| f.write(rsa_key.ssh_public_key)}
+          File.open(@private_key_path,'w'}{|f| f.write(rsa_key.rsa_private_key)}
+        rescue Exception => ex
+          env.ui.error "Error generating keypair : #{ex}"
+        end
     end
 
   end
