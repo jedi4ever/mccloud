6764f0c566f4a7ef6134f871689bf0860dc5a871
Patrick Debois <Patrick.Debois@jedi.be>
2011-12-01 08:44:16 +0100

    whitespace cleanup

--- lib/mccloud/provider/core/forwarding.rb
+++ lib/mccloud/provider/core/forwarding.rb
@@ -2,10 +2,10 @@ class Forwarding
   attr_accessor :name
   attr_accessor :local
   attr_accessor :remote
-    
+
   attr_accessor :namespace
-  
-  
+
+
   def initialize(name,local,remote)
     @name=name
     @local=local
