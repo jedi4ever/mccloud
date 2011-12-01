f1e06492c9b7376d82953eb4f1ea2fbdcf43338a
Patrick Debois <Patrick.Debois@jedi.be>
2011-12-01 08:45:09 +0100

    more whitespace

--- lib/mccloud/util/rsync.rb
+++ lib/mccloud/util/rsync.rb
@@ -1,14 +1,14 @@
 require "pty"
 module Mccloud
-	module Util
-		def self.rsync(path,vm,instance)
-		  unless !File.exists?(path)
-		    puts "[#{vm.name}] - rsyncing #{path}"
-			  command="rsync  --exclude '.DS_Store' --delete  -az -e 'ssh -p 22 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i \"#{vm.private_key}\"' '#{path}/' '#{vm.user}@#{instance.public_ip_address}:/tmp/#{File.basename(path)}/'"
-			else
+  module Util
+    def self.rsync(path,vm,instance)
+      unless !File.exists?(path)
+        puts "[#{vm.name}] - rsyncing #{path}"
+        command="rsync  --exclude '.DS_Store' --delete  -az -e 'ssh -p 22 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i \"#{vm.private_key}\"' '#{path}/' '#{vm.user}@#{instance.public_ip_address}:/tmp/#{File.basename(path)}/'"
+      else
         puts "[#{vm.name}] - rsync error: #{path} does no exist"
         exit
-		  end
+      end
     begin
       PTY.spawn( command ) do |r, w, pid|
         begin
@@ -18,9 +18,9 @@ module Mccloud
      end
    rescue PTY::ChildExited => e
       puts "The child process exited!"
-			   end
-			#Kernel.exec(command)
-		end
-	end
+         end
+      #Kernel.exec(command)
+    end
+  end
 end
 
