2c2b6887c6eef6d1e2c38c0f4df265dc5df11808
Patrick Debois <Patrick.Debois@jedi.be>
2011-12-03 13:47:53 +0100

    check for missing file before scp

--- lib/mccloud/provider/aws/vm/scp.rb
+++ lib/mccloud/provider/aws/vm/scp.rb
@@ -1,15 +1,18 @@
 module Mccloud::Provider
   module Aws
     module VmCommand
-         
+
       def transfer(src,dest)
         scp(src,dest)
       end
-             
-       def scp(src,dest) 
+
+       def scp(src,dest)
+         unless File.exists?(src)
+          raise Mccloud::Error,"scp failed: #{src} does not exist"
+         end
          @raw.scp(src,dest)
-      end 
-      
+      end
+
        end #module
       end #module
     end #module
