34165489bc285442912d1d6355c1476b34a0f204
Patrick Debois <Patrick.Debois@jedi.be>
2011-12-08 15:32:47 +0100

    adapted sample bootstrap

--- samples/bootstrap-ubuntu-system.sh
+++ samples/bootstrap-ubuntu-system.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 apt-get update
 apt-get -y install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert
 cd /tmp
-test ! -f rubygems-1.3.7 && wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
+test ! -f rubygems-1.3.7.tgz && wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
 test -f rubygems-1.3.7.tgz && tar zxf rubygems-1.3.7.tgz
-cd rubygems-1.3.7 && ruby setup.rb --no-format-executable
-gem install chef --no-ri --no-rdoc
-gem install puppet --no-ri --no-rdoc
+
+gem --version |grep 1.3.7 | wc -l |grep 0 && cd rubygems-1.3.7 && ruby setup.rb --no-format-executable
+
+gem list chef|grep chef|wc -l | grep 0 && gem install chef --no-ri --no-rdoc
+gem list puppet|grep puppet|wc -l | grep 0 && gem install puppet --no-ri --no-rdoc
+
+useradd puppet
+
+echo "bootstrap finished"
