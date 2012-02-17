641afeeb872e5d01dfe84ffb6364952cbcddc390
Patrick Debois <Patrick.Debois@jedi.be>
2012-02-17 10:55:28 +0100

    Cleanup vm for script

--- lib/mccloud/provider/script/vm.rb
+++ lib/mccloud/provider/script/vm.rb
@@ -1,13 +1,9 @@
 require 'mccloud/provider/core/vm'
 
-require 'mccloud/provider/script/vm/status'
-
 module Mccloud::Provider
   module Script
 
     class Vm < ::Mccloud::Provider::Core::Vm
-
-      include Mccloud::Provider::Script::VmCommand
 
       attr_accessor :ip_address
       attr_accessor :variables
