b993a2d8706616e82b22dc93336365a44cbb53cb
Patrick Debois <Patrick.Debois@jedi.be>
2012-03-27 16:11:57 +0200

    added vm list subcommand

--- lib/mccloud/command/vm.rb
+++ lib/mccloud/command/vm.rb
@@ -8,6 +8,14 @@ module Mccloud
         env.config.vms.define(vm_name,definition_name)
       end
 
+      desc "list", "list all known vms"
+      def list
+        printf("%-30s|%-20s|%-10s\n","alias","provider","ip-address")
+        env.config.vms.sort.each do |name,vm|
+          printf("%-30s|%-20s|%-10s\n",name,vm.provider.name,vm.ip_address)
+        end
+      end
+
     end #Class
 
   end #Module
