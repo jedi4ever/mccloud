5213ea29b2c55c486001da61718953ec4eb6791e
Patrick Debois <Patrick.Debois@jedi.be>
2012-05-09 17:30:57 +0200

    avoid empty scp_options

--- lib/mccloud/provider/aws/vm/scp.rb
+++ lib/mccloud/provider/aws/vm/scp.rb
@@ -6,14 +6,14 @@ module Mccloud::Provider
         scp(src,dest)
       end
 
-       def scp(local_path, remote_path, upload_options = {})
+       def scp(local_path, remote_path, scp_options = {})
          unless File.exists?(local_path)
           raise Mccloud::Error,"scp failed: #{local_path} does not exist"
          end
          #@raw.scp(src,dest)
          scp_options[:key_data] = [@raw.private_key] if @raw.private_key
 
-         Fog::SCP.new(self.ip_address, @raw.username, scp_options).upload(local_path, remote_path, upload_options)
+         Fog::SCP.new(self.ip_address, @raw.username, scp_options).upload(local_path, remote_path, {})
       end
 
        end #module
