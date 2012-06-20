e527fcc3023a8058cb7e6f5080cb1e308491dea4
Patrick Debois <Patrick.Debois@jedi.be>
2012-06-20 16:08:53 -0700

    show private ip address in vm list

--- lib/mccloud/command/vm.rb
+++ lib/mccloud/command/vm.rb
@@ -11,13 +11,13 @@ module Mccloud
       desc "list", "list all known vms"
       def list
         vms = env.config.vms.sort
-        printf("%-30s|%-20s|%-10s\n","alias","provider","ip-address")
+        printf("%-30s|%-20s|%-15s|%-15s\n","alias","provider","ip-address","private-address")
         80.times do 
           print '-'
         end
         puts
         vms.each do |name,vm|
-          printf("%-30s|%-20s|%-10s\n",name,vm.provider.name,vm.ip_address)
+          printf("%-30s|%-20s|%-15s|%-15s\n",name,vm.provider.name,vm.ip_address,vm.private_ip_address)
         end
       end
 
