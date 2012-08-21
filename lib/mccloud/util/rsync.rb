228d35032fca7c002ba8261ea8bd806dcc663279
tkn <tkn@zuehlke.com>
2012-08-21 11:11:31 +0200

    GH-17: use `Kernel.exec` instead of `PTY.spawn` because pty is not available on a windows Ruby

--- lib/mccloud/util/rsync.rb
+++ lib/mccloud/util/rsync.rb
@@ -1,24 +1,13 @@
-require "pty"
 module Mccloud
   module Util
     def self.rsync(path,vm,instance)
       unless !File.exists?(path)
         env.logger.info "[#{vm.name}] - rsyncing #{path}"
         command="rsync  --exclude='.DS_Store' --exclude='.git' --exclude='.hg' --delete --delete-excluded  -az -e 'ssh -p 22 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i \"#{vm.private_key}\"' '#{path}/' '#{vm.user}@#{instance.public_ip_address}:/tmp/#{File.basename(path)}/'"
+        Kernel.exec(command)
       else
         raise Mccloud::Error, "[#{vm.name}] - rsync error: #{path} does no exist"
       end
-    begin
-      PTY.spawn( command ) do |r, w, pid|
-        begin
-          r.each { |line| print line;}
-       rescue Errno::EIO
-       end
-     end
-   rescue PTY::ChildExited => e
-      puts "The child process exited!"
-         end
-      #Kernel.exec(command)
     end
   end
 end
